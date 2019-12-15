b6411a19b304ef7a14f46d1cdf62d178558a1197
Krzysztof Kaflowski <kkaflowskik@gmail.com>
2019-12-15 21:09:25 +0100

    Add files via upload
    Funkcje kary w lab_no=3 i lab_part=1, pierwsza jest zewnetrzna a druga jest wewnetrzna

--- main.cpp
+++ main.cpp
@@ -14,56 +14,62 @@ int main()
 		cout << "LAB NUMBER " << LAB_NO << endl;
 		cout << "LAB PART " << LAB_PART << endl << endl;
 #if LAB_NO==1
-		matrix Y0 = matrix(new double[3]{ 5,1,10 }, 3);
-		matrix *Y = solve_ode(0, 1, 1000, Y0);
+		matrix Y0 = matrix(new double[3]{ 5,1,10 }, 3); //5 m3 w A, 1 m3 w B , 10 stopni celsjusza w B
+		matrix *Y = solve_ode(0, 1, 1000, Y0); // t0, krok_czasu, czas_symulacji
 		ofstream S("sim_t.csv");
 		S << Y[0];
 		S.close();
-		S.open("sim_t.csv");
+		S.open("sim_y.csv");
 		S << Y[1];
 		S.close();
-		
+
 #elif LAB_NO==2
-
 #if LAB_PART == 1
-
-		double x0, d = 1, alfa = 1.5, epsilon = 1e-5, gamma = 1e-200;
+		double x0, d = 1, alfa = 1.410, epsilon = 1e-5, gamma = 1e-200;
 		int Nmax = 1000;
 		random_device R;
+		/*ofstream file("outputFile.txt");
+		for (int i = 0; i < 100; i++) {
+			x0 = 200.0*R() / R.max() - 100;	
+			file << x0 << ";";
+			double *ab = expansion(x0, d, alfa, Nmax);
+			file << ab[0] << ';' << ab[1] << ";" << ab[2]<< ";";
+			solution::clear_calls();
 
-		x0 =200.0 * R() / R.max() - 100; // trzeba przejsc na double
-		//x0 = 80;
+			solution opt_F = fib(ab[0], ab[1], epsilon);
+			file << opt_F.x << opt_F.y << solution::f_calls << ";" << ";";
+			solution::clear_calls();
+
+			solution opt_L = lag(ab[0], ab[1], epsilon, gamma, Nmax);
+			file << opt_L.x << opt_L.y << solution::f_calls << ";" << ";"<<endl;
+			solution::clear_calls;
+		}
+		file.close();*/
+		/*x0 = 200.0*R() / R.max() - 100;
 		cout << x0 << endl << endl;
-
 		double *ab = expansion(x0, d, alfa, Nmax);
-		cout << ab[0] << "\t" << ab[1] << endl;
-		cout << solution::f_calls << endl << endl;
-		solution::clear_calls();
+		cout << ab[0] << '\t' << ab[1] << endl << endl;
+		solution::clear_calls();*/
 
+		double ab[2]{ -100,100 };
 		solution opt_F = fib(ab[0], ab[1], epsilon);
 		cout << opt_F << endl << endl;
 		solution::clear_calls();
 
+		ab[0] = -100;
+		ab[1] = 100;
 		solution opt_L = lag(ab[0], ab[1], epsilon, gamma, Nmax);
 		cout << opt_L << endl << endl;
-		solution::clear_calls();
-
-
-
+		solution::clear_calls;
 #elif LAB_PART == 2
-
 		double x0, d = 1e-4, alfa = 1.5, epsilon = 1e-10, gamma = 1e-200;
 		int Nmax = 1000;
 		random_device R;
-
-		x0 = 200.0 * R() / R.max() - 100; // trzeba przejsc na double
-										  //x0 = 80;
 		x0 = (1e-2 - 1e-4)*R() / R.max() + 1e-4;
+		//x0 = 0.0016;
 		cout << x0 << endl << endl;
-
 		double *ab = expansion(x0, d, alfa, Nmax);
-		cout << ab[0] << "\t" << ab[1] << endl;
-		cout << solution::f_calls << endl << endl;
+		cout << ab[0] << '\t' << ab[1] << endl << endl;
 		solution::clear_calls();
 
 		solution opt_F = fib(ab[0], ab[1], epsilon);
@@ -72,77 +78,137 @@ int main()
 
 		solution opt_L = lag(ab[0], ab[1], epsilon, gamma, Nmax);
 		cout << opt_L << endl << endl;
-		solution::clear_calls();
-
+		solution::clear_calls;
 #endif
 
 #elif LAB_NO==3
+#if LAB_PART == 1
 
-#if LAB_PART==1
-		matrix x0(2, 1), s0(2, 1);
-		double s = 0.1, epsilon = 1e-3, alfa, beta;
+		ofstream File("opt34_test.csv");
+		locale loc("");
+		File.imbue(loc);
+		File << "x0;x1;MHJ;;;;;MR;;;;\n";
+		File << "x0;x1;x1*;x2*;y*;lwywolan;min;x1*;x2*;y*;lwywolan;min\n";
+
+		matrix x0(2,1);
 		int Nmax = 1000;
+		double alfa, beta, epsilon = 1e-3, s = 0.136558;
 		random_device R;
 
-		x0(0) = 2.0*R() / R.max() - 1;
-		x0(1) = 2.0*R() / R.max() - 1;
+		int fcalls1, fcalls2;
 
+		for (int i = 0; i < 1; i++)
+		{
+			x0(0) = 2.0*R() / R.max() - 1;
+			x0(1) = 2.0*R() / R.max() - 1;
+
+			x0(0) = -0.413967;
+			x0(1) = -0.994215;
+
+
+
+			cout << x0 << endl << endl;
+			alfa = 0.5;
+			solution opt_HJ = HJ(x0, s, alfa, epsilon, Nmax);
+			fcalls1 = solution::f_calls;
+			File << x0(0) << ";" << x0(1) << ";" << opt_HJ.x(0) << ";" << opt_HJ.x(1) << ";" << opt_HJ.y << fcalls1 << ";;";
+
+			cout << opt_HJ << endl << endl;
+			solution::clear_calls();
+			cout << endl;
+
+			alfa = 2;
+			beta = 0.5;
+			matrix s0(2, 1);
+			s0(0) = s;
+			s0(1) = s;
+			solution opt_R = Rosen(x0, s0, alfa, beta, epsilon, Nmax);
+			fcalls2 = solution::f_calls;
+			File << opt_R.x(0) << ";" << opt_R.x(1) << ";" << opt_R.y << fcalls2 << ";\n";
+
+			cout << opt_R << endl << endl;
+			solution::clear_calls();
+			cout << endl;
+
+			cout << endl << endl;
+			/*cout << x0 << opt_HJ;
+			cout << fit1 << "  ";
+			cout << opt_R;
+			cout << fit2 << endl;*/
+		}
+
+#elif LAB_PART == 2
+		matrix x0(2, 1);
+		int Nmax = 1000;
+		double alfa, beta, epsilon = 1e-3, s = 0.32658;
+		random_device R;
+		x0(0) = 10.0*R() / R.max();
+		x0(1) = 10.0*R() / R.max();
 		cout << x0 << endl << endl;
 		alfa = 0.5;
 		solution opt_HJ = HJ(x0, s, alfa, epsilon, Nmax);
 		cout << opt_HJ << endl << endl;
+		
 		solution::clear_calls();
+
 		alfa = 2;
 		beta = 0.5;
+		matrix s0(2, 1);
 		s0(0) = s;
 		s0(1) = s;
 		solution opt_R = Rosen(x0, s0, alfa, beta, epsilon, Nmax);
 		cout << opt_R << endl << endl;
 		solution::clear_calls();
 
-#elif LAB_PART==2
+#elif LAB_PART == 3
+		matrix Y0 = matrix(new double[2]{ 0,0 }, 2); //polozenie 0, predkoscramienia 0
+		matrix P = matrix(new double[2]{ 2.7688, 3.18129 }, 2); //k1, k2
+		matrix* Y = solve_ode(0, 0.1, 100, Y0, P); // t0, krok_czasu, czas_symulacji
+		ofstream S("sim_y.csv");
+		S << Y[1];
+		S.close();
 
-		matrix x0(2, 1), s0(2, 1);
-		double s = 0.5, epsilon = 1e-3, alfa, beta;
 		int Nmax = 1000;
+		double alfa, beta, epsilon = 1e-3, s = 0.1;
 		random_device R;
 
-		x0(0) = 10.0*R() / R.max() - 1;
-		x0(1) = 10.0*R() / R.max() - 1;
-
-		cout << x0 << endl << endl;
+		S.open("Hpart3.csv");
 		alfa = 0.5;
-		solution opt_HJ = HJ(x0, s, alfa, epsilon, Nmax);
+		solution opt_HJ = HJ(Y0, s, alfa, epsilon, Nmax);
 		cout << opt_HJ << endl << endl;
-		solution::clear_calls();
-		alfa = 2;
-		beta = 0.5;
-		s0(0) = s;
-		s0(1) = s;
-		solution opt_R = Rosen(x0, s0, alfa, beta, epsilon, Nmax);
-		cout << opt_R << endl << endl;
-		solution::clear_calls();
+		S << opt_HJ.x(0) << ";" << opt_HJ.x(1) << ";" << opt_HJ.y << ";;";
 
-#endif // LAB_PART==1 i LAB_PART == 2
+		S.close();
 
+
+#endif
 
 #elif LAB_NO==4
+
 	#if LAB_PART == 1
 
-		//kara zewnetrzna
-		matrix x0(2);
+	//kara zewnetrzna
+	matrix x0(2);
 
-		double a, c = 1, dc = 2, epsilon = 1e-4;
-		int Nmax = 5000;
-		random_device R;
+	double a, c = 1, dc = 2, epsilon = 1e-4;
+	double a_wew, c_wew = 10, dc_wew = 0.5, epsilon_wew = 1e-4;
+	int Nmax = 5000;
+	random_device R;
 
-		//a przyjmuje 3 wartosci
-		//wyznaczamy punkt poczatkowy
-		
-		//w przypadku kary zewnetrznej, caly simpleks moze lezec poza obszarem
-		//w karze wewnetrznej nie moze byc poza, (przynajmniej jeden musi byc w srodku?)
+	//a przyjmuje 3 wartosci
+	//wyznaczamy punkt poczatkowy
 
-		//wartosc funkcji zmodyfikowanej wynosi nieskonczonosc, dlatego nie moze (? xD)
+	//w przypadku kary zewnetrznej, caly simpleks moze lezec poza obszarem
+	//w karze wewnetrznej nie moze byc poza, (przynajmniej jeden musi byc w srodku?)
+
+	//wartosc funkcji zmodyfikowanej wynosi nieskonczonosc, dlatego nie moze (? xD)
+	ofstream S("kara_zewnetrzna_a_4.csv");
+	S << "x0(0);x0(1);x_opt(0);x_opt(1);r;y;f_calls;" << endl;
+
+	ofstream S2("kara_wewnetrzna_a_4.csv");
+	S2 << "x0(0);x0(1);x_opt(0);x_opt(1);r;y;f_calls;" << endl;
+
+	for (int i = 0; i <= 100; i++) {
 
 		a = 4;
 		do {
@@ -150,59 +216,71 @@ int main()
 			x0(1) = 4.0 * R() / R.max() + 1;
 		} while (sqrt(pow(x0(0), 2) + pow(x0(1), 2)) > a);
 
-			cout << x0 << endl;
-			cout << sqrt(pow(x0(0), 2) + pow(x0(1), 2)) << endl;
+		//kara zewnetrzna
+		solution opt = pen(x0, c, dc, epsilon, Nmax, a);
 
-			solution opt = pen(x0, c, dc, epsilon, Nmax, a);
+		double r_opt = sqrt(pow(opt.x(0), 2) + pow(opt.x(1), 2));
 
-			cout << opt << endl;
-			cout << sqrt(pow(x0(0), 2) + pow(x0(1), 2)) << endl;
+		//wpisywanie do pliku
+		S << x0(0) << ";" << x0(1) << ";" << opt.x(0) << ";" << opt.x(1) << ";" << r_opt << ";" << opt.y <<";"<<solution::f_calls<<";"<< endl;
 
-			solution::clear_calls();
+		solution::clear_calls();
+
+		//kara wewnetrzna
+		solution opt_wew = pen(x0, c_wew, dc_wew, epsilon_wew, Nmax, a);
+		double r_opt_wew = sqrt(pow(opt_wew.x(0), 2) + pow(opt_wew.x(1), 2));
+		S2 << x0(0) << ";" << x0(1) << ";" << opt_wew.x(0) << ";" << opt_wew.x(1) << ";" << r_opt_wew << ";" << opt_wew.y << ";" << solution::f_calls << ";" << endl;
+
+
+		solution::clear_calls();
+
+	}
+
 
 
 
 	#elif LAB_PART == 2
 
-		//wspolczynnikk c przy karze wewnterznej 
+	//wspolczynnikk c przy karze wewnterznej 
 
-			matrix x0(2);
+	matrix x0(2);
 
-			double a, c = 10, dc = 0.5, epsilon = 1e-4;
-			int Nmax = 10000;
-			random_device R;
+	double a, c = 10, dc = 0.5, epsilon = 1e-4;
+	int Nmax = 10000;
+	random_device R;
 
-			//a przyjmuje 3 wartosci
-			//wyznaczamy punkt poczatkowy
+	//a przyjmuje 3 wartosci
+	//wyznaczamy punkt poczatkowy
 
-			//w przypadku kary zewnetrznej, caly simpleks moze lezec poza obszarem
-			//w karze wewnetrznej nie moze byc poza, (przynajmniej jeden musi byc w srodku?)
+	//w przypadku kary zewnetrznej, caly simpleks moze lezec poza obszarem
+	//w karze wewnetrznej nie moze byc poza, (przynajmniej jeden musi byc w srodku?)
 
-			//wartosc funkcji zmodyfikowanej wynosi nieskonczonosc, dlatego nie moze (? xD)
+	//wartosc funkcji zmodyfikowanej wynosi nieskonczonosc, dlatego nie moze (? xD)
 
-			a = 4;
-			do {
-				x0(0) = 4.0 * R() / R.max() + 1;
-				x0(1) = 4.0 * R() / R.max() + 1;
-			} while (sqrt(pow(x0(0), 2) + pow(x0(1), 2)) > a);
+	a = 4;
+	do {
+		x0(0) = 4.0 * R() / R.max() + 1;
+		x0(1) = 4.0 * R() / R.max() + 1;
+	} while (sqrt(pow(x0(0), 2) + pow(x0(1), 2)) > a);
 
-			cout << x0 << endl;
-			cout << sqrt(pow(x0(0), 2) + pow(x0(1), 2)) << endl;
+	cout << x0 << endl;
+	cout << sqrt(pow(x0(0), 2) + pow(x0(1), 2)) << endl;
 
-			solution opt = pen(x0, c, dc, epsilon, Nmax, a);
+	solution opt = pen(x0, c, dc, epsilon, Nmax, a);
 
-			cout << opt << endl;
-			cout << sqrt(pow(x0(0), 2) + pow(x0(1), 2)) << endl;
+	cout << opt << endl;
+	cout << sqrt(pow(x0(0), 2) + pow(x0(1), 2)) << endl;
 
-			solution::clear_calls();
+	solution::clear_calls();
 
 
 	#elif LAB_PART == 3
-		//problem rzeczywisty
-		
+	//problem rzeczywisty
 
-#endif // LAB_PART == 1
 
+	
+	
+#endif
 
 #elif LAB_NO==5
 		
@@ -219,32 +297,3 @@ int main()
 	system("pause");
 	return 0;
 }
-
-//lab3
-//w przypadku funkcji testowej, 3 dlugosci kroki, po 100 razy, wyniki w tabeli 1
-//w tabeli 2 tylko te ktore byly sukcesem, minimum globalne, jest wartosc 0, 
-
-//liczyc ile razy skonczylo sie minimum globalne
-// drugie minimum jest w odlgeosci przynajmniej 0.5
-//dla jednego wybranego przypadku, narysowac wykres, naniesc rozwiazanie w kazdej iteracji, w HJ punkty bazowe, W rosen modyfikacja kroku, kierunku
-//przed zmiana kierunku wypisywac x
-
-//ramie robota
-//tylko raz
-//narysowac wykres dla k1 i k2, (P[0],P 1 )
-//jak zmienia sie alfa i omega
-
-
-
-//lab2
-//olac NAN, liczyc srednie z pozostalych
-//jak sa ujemne to tez uwzglednic. ( objetosc A), 10e-6
-
-
-//zakladamy ze moment sily jest staly
-//zakladamy ze przez 1s stan wody jest staly
-
-
-// ok 24 cm2 zbiornik
-
-
